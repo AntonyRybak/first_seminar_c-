307a119f8b822fad6b68f6e763598f921b38094b
Anton Rybak <toxa-wow@mail.ru>
2022-08-19 23:30:49 +0300

    first_task_done

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Напишите первое число ");
+int numberone = int.Parse(Console.ReadLine());
+Console.Write("Напишите второе число ");
+int numbertwo = int.Parse(Console.ReadLine());
+if (numberone > numbertwo)
+{
+    Console.WriteLine("Первое число больше, а второе число меньше");
+}
+else
+{
+    Console.WriteLine("Второе число больше, а первое число меньше");
+}
