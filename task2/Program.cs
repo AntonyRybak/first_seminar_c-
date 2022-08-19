b18c6c6374b2cac79bac78f3d98c23583de46f2c
Anton Rybak <toxa-wow@mail.ru>
2022-08-20 00:12:19 +0300

    second task done

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int numberone = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numbertwo = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int numberthree = int.Parse(Console.ReadLine());
+int max = 0;
+if (numberone > numbertwo)
+{
+    max = numberone;
+}
+if (numbertwo > numberone)
+{
+    max = numbertwo;
+}
+if (numberthree > max)
+{
+    max = numberthree;
+}
+Console.Write($"Наибольшее число {max}");
+
+
+
+
