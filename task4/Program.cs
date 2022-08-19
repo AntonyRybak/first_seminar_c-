fe3a5e7ed6d424c5cae6d9803e776bd6bda8a412
Anton Rybak <toxa-wow@mail.ru>
2022-08-20 01:56:33 +0300

    add fourth task

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+int lenght = n - 1;
+while (i < lenght) 
+{
+    i += 2;
+    Console.Write($"{i}, ");  
+}
+
+    
+
+
+
